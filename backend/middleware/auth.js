00f00f3dc20ff08919b66a085afa3f42b23d6e86
Tammana <tanutmna@gmail.com>
2026-06-20 01:34:24 +0530

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,126 @@
+// AUTH , IS STUDENT , IS INSTRUCTOR , IS ADMIN
+
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+
+// ================ AUTH ================
+// user Authentication by checking token validating
+exports.auth = (req, res, next) => {
+    try {
+        // extract token by anyone from this 3 ways
+        const token = req.body?.token || req.cookies.token || req.header('Authorization').replace('Bearer ', '');
+
+        // if token is missing
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: 'Token is Missing'
+            });
+        }
+        // verify token
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decode;
+        }
+        catch (error) {
+            console.log('Error while decoding token');
+            console.log(error);
+            return res.status(401).json({
+                success: false,
+                error: error.message,
+                messgae: 'Error while decoding token'
+            })
+        }
+        // go to next middleware
+        next();
+    }
+    catch (error) {
+        console.log('Error while token validating');
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            messgae: 'Error while token validating'
+        })
+    }
+}
+
+
+
+
+
+// ================ IS STUDENT ================
+exports.isStudent = (req, res, next) => {
+    try {
+        // console.log('User data -> ', req.user)
+        if (req.user?.accountType != 'Student') {
+            return res.status(401).json({
+                success: false,
+                messgae: 'This Page is protected only for student'
+            })
+        }
+        // go to next middleware
+        next();
+    }
+    catch (error) {
+        console.log('Error while cheching user validity with student accountType');
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            messgae: 'Error while cheching user validity with student accountType'
+        })
+    }
+}
+
+
+// ================ IS INSTRUCTOR ================
+exports.isInstructor = (req, res, next) => {
+    try {
+        // console.log('User data -> ', req.user)
+        if (req.user?.accountType != 'Instructor') {
+            return res.status(401).json({
+                success: false,
+                messgae: 'This Page is protected only for Instructor'
+            })
+        }
+        // go to next middleware
+        next();
+    }
+    catch (error) {
+        console.log('Error while cheching user validity with Instructor accountType');
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            messgae: 'Error while cheching user validity with Instructor accountType'
+        })
+    }
+}
+
+
+// ================ IS ADMIN ================
+exports.isAdmin = (req, res, next) => {
+    try {
+        // console.log('User data -> ', req.user)
+        if (req.user.accountType != 'Admin') {
+            return res.status(401).json({
+                success: false,
+                messgae: 'This Page is protected only for Admin'
+            })
+        }
+        // go to next middleware
+        next();
+    }
+    catch (error) {
+        console.log('Error while cheching user validity with Admin accountType');
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+            messgae: 'Error while cheching user validity with Admin accountType'
+        })
+    }
+}
+
+
